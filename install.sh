893888c0f0c7af2f4b2d4cae3519f039f07a09f2
Zwelch <zwelchmobile@googlemail.com>
2023-02-07 22:54:46 +0100

    first commit

---
+++ install.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+#
+
+## Variables
+
+function pacman_init ()
+{
+    echo "#############"
+    echo "#Pacman Init#"
+    echo "#############"
+    pacman-key --init   
+    pacman-key --populate archlinux
+}
+
+function diskparts ()
+{
+    echo "#################"
+    echo "#Disk Partitions#"
+    echo "#################"
+    fdisk -l
+    echo "Enter disk name for installation: "
+    read disk
+    sfdisk /dev/$disk < sdx.sfdisk
+    dn=${disk}1
+    mkfs.fat -F32 /dev/$dn
+    dn=${disk}2
+    mkswap /dev/$dn
+    swapon /dev/$dn
+    dn=${disk}3
+    mkfs.btrfs /dev/$dn
+    mount /dev/$dn/mnt
+    cd /mnt
+    read
+}
+
+
+echo "####################"
+echo "#Installtion Script#"
+echo "####################"
+echo " "
+echo "Press enter to start..."
+read
+## Sequencer
+pacman_init
+diskparts
