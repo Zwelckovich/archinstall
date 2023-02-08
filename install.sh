b0773db2c11b4404173a34baa504ca7a0fcaf375
Zwelckovich <54358761+Zwelckovich@users.noreply.github.com>
2023-02-08 12:54:07 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -147,8 +147,8 @@ function i3_install ()
     popd
     rm -rf yay
     pacman --noconfirm -Syu
-    pacman --noconfirm -S xorg lightdm lightdm-webkit2-greeter i3 dmenu feh archlinux-wallpaper xfce4-terminal picom brave firefox pacman-contrib alsa-utils pulseaudio pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-jack pavucontrol 
-    yay --noconfirm -S timeshift timeshift-autosnap zramd
+    pacman --noconfirm -S xorg lightdm lightdm-webkit2-greeter i3 dmenu feh archlinux-wallpaper xfce4-terminal picom firefox pacman-contrib alsa-utils pulseaudio pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-jack pavucontrol 
+    yay --noconfirm -S timeshift timeshift-autosnap zramd brave-bin
     sudo paccache -r
     sudo systemctl enable lightdm
     sudo systemctl enable --now zramd
