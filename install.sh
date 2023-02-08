d5c05d63fb417e4359d43d16d7c9fb0aa701e207
Zwelckovich <54358761+Zwelckovich@users.noreply.github.com>
2023-02-08 09:21:39 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,6 +21,7 @@ function diskparts ()
     fdisk -l
     echo "Enter disk name for installation: "
     read disk
+    umount /dev/${disk}?*
     sgdisk --zap-all /dev/$disk
     sgdisk -n 1:0:+300M -n 2:0:+8G -n 3:0:0 -t 1:ef00 -t 2:8200 /dev/$disk -p
     dn=${disk}1
@@ -31,7 +32,6 @@ function diskparts ()
     dn=${disk}3
     mkfs.btrfs /dev/$dn
     mount /dev/$dn /mnt
-    cd /mnt
     read
 }
 
