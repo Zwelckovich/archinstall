6f776c3784d7e5d552fe01f1bd854021057ec703
Zwelckovich <54358761+Zwelckovich@users.noreply.github.com>
2023-02-08 11:37:03 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -88,12 +88,12 @@ function base_config ()
     variable="en_US.UTF-8 UTF-8"
     arch-chroot /mnt sed -i "/^#$variable/ c$variable" /etc/locale.gen
     arch-chroot /mnt locale-gen
-    arch-chroot /mnt echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-    arch-chroot /mnt echo "KEYMAP=de"  >> /etc/vconsole.conf
-    arch-chroot /mnt echo "zwelcharch" >> /etc/hostname
-    arch-chroot /mnt echo "127.0.0.1	localhost" >> /etc/hosts
-    arch-chroot /mnt echo "::1		localhost" >> /etc/hosts
-    arch-chroot /mnt echo "127.0.1.1	zwelcharch.localdomain	zwelcharch" >> /etc/hosts
+    arch-chroot /mnt bash -c 'echo "LANG=en_US.UTF-8" >> /etc/locale.conf'
+    arch-chroot /mnt bash -c 'echo "KEYMAP=de"  >> /etc/vconsole.conf'
+    arch-chroot /mnt bash -c 'echo "zwelcharch" >> /etc/hostname'
+    arch-chroot /mnt bash -c 'echo "127.0.0.1	localhost" >> /etc/hosts'
+    arch-chroot /mnt bash -c 'echo "::1		localhost" >> /etc/hosts'
+    arch-chroot /mnt bash -c 'echo "127.0.1.1	zwelcharch.localdomain	zwelcharch" >> /etc/hosts'
     echo "### ROOT PASSWORD ###"
     arch-chroot /mnt passwd
     variable="Color"
