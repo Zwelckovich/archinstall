61811416b0d2a4e2a10c3e7eb3715d565c5560b1
Zwelckovich <54358761+Zwelckovich@users.noreply.github.com>
2023-02-08 21:50:49 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -150,7 +150,6 @@ function i3_install ()
     sudo pacman --noconfirm -Syu
     sudo pacman --noconfirm -S xorg lightdm lightdm-webkit2-greeter i3 dmenu feh archlinux-wallpaper xfce4-terminal picom firefox pacman-contrib alsa-utils pulseaudio pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-jack pavucontrol 
     yay --noconfirm -S timeshift timeshift-autosnap zramd
-    yay --noconfirm -S brave-bin
     sudo systemctl enable lightdm
     sudo systemctl enable --now zramd
     variable="greeter-session=example-gtk"
