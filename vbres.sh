8a8ce869c88af5d28c216a3e4400b8e87df7f072
Zwelch <zwelchmobile@googlemail.com>
2023-02-20 07:58:47 +0100

    Update

--- vbres.sh
+++ vbres.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
-res="1920 1080 60"
+#res="1920 1080 60"
+res="2560 1440 60"
 variable=$(cvt $res | grep Modeline | cut -d ' ' -f 2-)
 monitor=$(xrandr | grep Virtual | grep primary | awk '{print $1}')
 modename=$(cvt $res | grep Modeline | cut -d ' ' -f 2)
@@ -10,3 +11,4 @@ cstr=$(echo "xrandr --addmode $monitor $modename")
 eval "$cstr"
 cstr=$(echo "xrandr --output $monitor --primary --mode $modename --pos 0x0 --rotate normal")
 eval "$cstr"
+feh --bg-scale wallpaper/nebula.png
