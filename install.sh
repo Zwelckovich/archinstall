aec71adc2a2fcfecbc47402ff82174910a718ac9
Zwelch <zwelchmobile@googlemail.com>
2024-06-05 22:32:31 +0200

    bug fix

--- install.sh
+++ install.sh
@@ -413,7 +413,7 @@ function restore_dotfiles() {
     git pull
     popd
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions 
-    pushd ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestion
+    pushd ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     git pull
     popd
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
