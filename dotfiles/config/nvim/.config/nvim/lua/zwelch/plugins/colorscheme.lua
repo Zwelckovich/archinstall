7b2158db9c4d79d33cc4a7e9496f39552bd43ef3
Zwelch <zwelchmobile@googlemail.com>
2024-06-07 09:32:27 +0200

    finished nvim setup

--- dotfiles/config/nvim/.config/nvim/lua/zwelch/plugins/colorscheme.lua
+++ dotfiles/config/nvim/.config/nvim/lua/zwelch/plugins/colorscheme.lua
@@ -1,27 +1,28 @@
 return {
-  "catppuccin/nvim", 
-  name = "catppuccin", 
-  priority = 1000, 
-  config = function()
-    vim.cmd("colorscheme catppuccin-mocha")
-    require("catppuccin").setup({
-      integrations = {
-        telescope = {
-          enabled = true,
-          -- style = "nvchad"
-        },
-        nvimtree = true,
-        lsp_trouble = true,
-        alpha = true,
-        which_key = true,
-        indent_blankline = {
-          enabled = true,
-          scope_color = "", -- catppuccin color (eg. `lavender`) Default: text
-          colored_indent_levels = true,
-        },
-        mason = true,
-        cmp = true,
-      }
-    })
-  end
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		vim.cmd("colorscheme catppuccin-mocha")
+		require("catppuccin").setup({
+			integrations = {
+				telescope = {
+					enabled = true,
+					-- style = "nvchad"
+				},
+				nvimtree = true,
+				lsp_trouble = true,
+				alpha = true,
+				which_key = true,
+				indent_blankline = {
+					enabled = true,
+					scope_color = "maroon", -- catppuccin color (eg. `lavender`) Default: text
+					colored_indent_levels = true,
+				},
+				mason = true,
+				cmp = true,
+				gitsigns = true,
+			},
+		})
+	end,
 }
