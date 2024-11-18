f7e8cb462f8abdafd844367b3df84061bc0a8aaf
Zwelckovich <54358761+Zwelckovich@users.noreply.github.com>
2024-11-18 22:04:56 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -394,14 +394,62 @@ function i3_install() {
  	# Setup Nvidia if it was found
 	if [[ "$ISNVIDIA" == true ]]; then
 		echo -e "$CNT - Nvidia GPU support setup stage, this may take a while..."
+ 		 for SOFTWR in ${uninstall_nvidia_stage[@]}; do
+			uninstall_software $SOFTWR
+		done
+  
 		for SOFTWR in ${nvidia_stage[@]}; do
 			install_software $SOFTWR
 		done
 
-		# update config
-		sudo sed -i 's/MODULES=()/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
-		sudo mkinitcpio --config /etc/mkinitcpio.conf --generate /boot/initramfs-custom.img
-		echo -e "options nvidia-drm modeset=1" | sudo tee -a /etc/modprobe.d/nvidia.conf &>>$INSTLOG
+		# Check if the Nvidia modules are already added in mkinitcpio.conf and add if not
+		if grep -qE '^MODULES=.*nvidia. *nvidia_modeset.*nvidia_uvm.*nvidia_drm' /etc/mkinitcpio.conf; then
+		  echo "Nvidia modules already included in /etc/mkinitcpio.conf" 2>&1 | tee -a "$INSTLOG"
+		else
+		  sudo sed -Ei 's/^(MODULES=\([^\)]*)\)/\1 nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf 2>&1 | tee -a "$INSTLOG"
+		  echo "Nvidia modules added in /etc/mkinitcpio.conf"
+		fi
+		
+		sudo mkinitcpio -P 2>&1 | tee -a "$INSTLOG"
+		printf "\n\n\n"
+		
+		# Additional Nvidia steps
+		NVEA="/etc/modprobe.d/nvidia.conf"
+		if [ -f "$NVEA" ]; then
+		  printf "$COK Seems like nvidia-drm modeset=1 is already added in your system..moving on.\n"
+		  printf "\n"
+		else
+		  printf "\n"
+		  printf "$CNT Adding options to $NVEA..."
+		  sudo echo -e "options nvidia_drm modeset=1 fbdev=1" | sudo tee -a /etc/modprobe.d/nvidia.conf 2>&1 | tee -a "$INSTLOG"
+		  printf "\n"
+		fi
+		
+		# Additional for GRUB users
+		# Check if /etc/default/grub exists
+		if [ -f /etc/default/grub ]; then
+		    # Check if nvidia-drm.modeset=1 is present
+		    if ! sudo grep -q "nvidia-drm.modeset=1" /etc/default/grub; then
+		        # Add nvidia-drm.modeset=1 to GRUB_CMDLINE_LINUX_DEFAULT
+		        sudo sed -i -e 's/\(GRUB_CMDLINE_LINUX_DEFAULT=".*\)"/\1 nvidia-drm.modeset=1"/' /etc/default/grub
+		        echo "nvidia-drm.modeset=1 added to /etc/default/grub" 2>&1 | tee -a "$INSTLOG"
+		    fi
+		
+		    # Check if nvidia_drm.fbdev=1 is present
+		    if ! sudo grep -q "nvidia_drm.fbdev=1" /etc/default/grub; then
+		        # Add nvidia_drm.fbdev=1 to GRUB_CMDLINE_LINUX_DEFAULT
+		        sudo sed -i -e 's/\(GRUB_CMDLINE_LINUX_DEFAULT=".*\)"/\1 nvidia_drm.fbdev=1"/' /etc/default/grub
+		        echo "nvidia_drm.fbdev=1 added to /etc/default/grub" 2>&1 | tee -a "$INSTLOG"
+		    fi
+		
+		    # Regenerate GRUB configuration if any changes were made
+		    if sudo grep -q "nvidia-drm.modeset=1" /etc/default/grub || sudo grep -q "nvidia_drm.fbdev=1" /etc/default/grub; then
+		        sudo grub-mkconfig -o /boot/grub/grub.cfg
+		    fi
+		
+		else
+		    echo "/etc/default/grub does not exist"
+		fi
 	fi
  
 	echo -e "$CNT - Tools Stage Install"
