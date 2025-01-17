eecd739d6504e45c4b587cf954e6e952942b6f07
Zwelch <zwelchmobile@googlemail.com>
2025-01-17 11:35:03 +0100

    added luasnip

--- dotfiles/config/lazyvim/.config/nvim/lua/config/keymaps.lua
+++ dotfiles/config/lazyvim/.config/nvim/lua/config/keymaps.lua
@@ -5,3 +5,21 @@ local keymap = vim.keymap -- for conciseness
 
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 keymap.set("n", "<C-l>", "<c-g>u<Esc>[s1z=`]a<c-g>u", { desc = "Spell Correction" })
+
+local ls = require("luasnip")
+
+vim.keymap.set({ "i" }, "<C-K>", function()
+	ls.expand()
+end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<C-L>", function()
+	ls.jump(1)
+end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<C-J>", function()
+	ls.jump(-1)
+end, { silent = true })
+
+vim.keymap.set({ "i", "s" }, "<C-E>", function()
+	if ls.choice_active() then
+		ls.change_choice(1)
+	end
+end, { silent = true })
