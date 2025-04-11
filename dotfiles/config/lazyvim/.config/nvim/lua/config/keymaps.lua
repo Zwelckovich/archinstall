7be81d83b21d57b8db6eb3b191fffe50721d3b8e
Zwelch <zwelchmobile@googlemail.com>
2025-04-11 09:52:29 +0200

    changed harpoon config

--- dotfiles/config/lazyvim/.config/nvim/lua/config/keymaps.lua
+++ dotfiles/config/lazyvim/.config/nvim/lua/config/keymaps.lua
@@ -5,3 +5,8 @@ local keymap = vim.keymap -- for conciseness
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 keymap.set("n", "ö", "[", { desc = "Changed umlauts to brackets" })
 keymap.set("n", "ä", "]", { desc = "Changed umlauts to brackets" })
+
+local wk = require("which-key")
+wk.add({
+	{ "<leader>h", group = "harpoon", icon = "" },
+})
